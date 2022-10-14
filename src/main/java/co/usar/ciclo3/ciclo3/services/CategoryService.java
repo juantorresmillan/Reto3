bd118721ca756d3a17bae728b9914ff94e4c9095
Juan David Torres millan <113484397+juantorresmillan@users.noreply.github.com>
2022-10-14 17:52:01 -0500

    Update CategoryService.java

--- src/main/java/co/usar/ciclo3/ciclo3/services/CategoryService.java
+++ src/main/java/co/usar/ciclo3/ciclo3/services/CategoryService.java
@@ -38,7 +38,7 @@ public class CategoryService {
 
     public Category update(Category cat){
         Integer id = cat.getId();
-        if(id==null){
+        if(id!=null){
             Optional<Category> cataux = categoryRepository.getCategory(cat.getId());
             if(cataux.isPresent()){
                 if(cat.getDescription() != null){
